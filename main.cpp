4b9ac017e1446501c337f4395c80e38e60dde0bd
Julia Nitsch <julia.nitsch@gmail.com>
2022-10-09 14:39:48 +0000

    test_main

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    int x = 5+5;
+    std::cout << "x: " << x << std::endl;
+    return 0;
+}
